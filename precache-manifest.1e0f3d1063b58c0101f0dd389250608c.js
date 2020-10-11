947143f36dff752bebe27cbd2774590bf67d23d6
mpl1018 <44880660+mpl1018@users.noreply.github.com>
2020-10-12 00:57:34 +0200

    Updates

---
+++ precache-manifest.1e0f3d1063b58c0101f0dd389250608c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dc34ec423dc84a8b77cd5f49a896eeb",
+    "url": "/React_Covid-19_DashBoard/index.html"
+  },
+  {
+    "revision": "08bb067757c653b5ac60",
+    "url": "/React_Covid-19_DashBoard/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "1a35653d379311a5dc3d",
+    "url": "/React_Covid-19_DashBoard/static/css/main.c60e7cf7.chunk.css"
+  },
+  {
+    "revision": "08bb067757c653b5ac60",
+    "url": "/React_Covid-19_DashBoard/static/js/2.a5c5064b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React_Covid-19_DashBoard/static/js/2.a5c5064b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a35653d379311a5dc3d",
+    "url": "/React_Covid-19_DashBoard/static/js/main.b398ed61.chunk.js"
+  },
+  {
+    "revision": "c1e5fb1e3bc138b9c58a",
+    "url": "/React_Covid-19_DashBoard/static/js/runtime-main.d0b900b4.js"
+  }
+]);
